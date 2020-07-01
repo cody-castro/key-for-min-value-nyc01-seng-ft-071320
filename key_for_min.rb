61fd81201a6f5127e9e8376c40be4b743cd1cbdf
Cody C <codyicastro@gmail.com>
2020-07-01 22:47:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_val = nil
   min_key = nil
   name_hash.each do |key, value|
-    if min_val == nil || min_val < value
+    if min_val == nil || min_val < (value)
       min_val = value
       min_val = key
   end
